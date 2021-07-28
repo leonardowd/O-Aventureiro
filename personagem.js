8efc9b1aca28cb25f768aa9fb5c6d830a53ec06d
leonardo vinicius <leonardoviniciuswd@gmail.com>
2021-07-28 16:44:05 -0300

    criando o personagem

--- personagem.js
+++ personagem.js
@@ -1,8 +1,6 @@
 class personagem {
-    hp: "100",
-    dano: "10",
-    equipamento: "",
-    arma: function() {
-        set
+    constructor(hp, dano, equipamento) {
+        this.hp = 100;
+        this.dano = 10;
     }
 }
