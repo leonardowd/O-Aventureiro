d080c62e516ea625b5f6d271ffb15e1d0d62bc56
leonardo vinicius <leonardoviniciuswd@gmail.com>
2021-07-29 14:41:18 -0300

    alterando roteiro do jogo

---
+++ roteiro.js
@@ -0,0 +1,63 @@
+//Aqui se inicia a história do jogo, a partir do momento que o usuário clique no botão "Deseja continuar?". 
+function iniciar() {
+
+    let resposta; //variável criada para recolher a resposta do jogador
+
+//importando alguns elementos do HTML para serem modificados conforme o jogo avança.
+
+    //personagem principal:
+        let abaHp = document.getElementById('quantidadeHp');
+        let abaDano = document.getElementById('quantidadeDano');
+
+        //setando atributos iniciais do personagem//
+        hp = 100;
+        dano = 10;
+        abaHp.innerHTML = `Vida: ${hp}`;
+        abaDano.innerHTML = `Dano: ${dano}`;
+    //Inimigos:
+        let abaNomeInimigo = document.getElementById('nomeInimigo');
+        let abaHpInimigo = document.getElementById('quantidadeHpInimigo');
+        let abaDanoInimigo = document.getElementById('quantidadeDanoInimigo');
+
+    //Botões de ação:
+        let botoesAcao = document.getElementById('ocultarActionBar');
+        let botaoSim = document.getElementById('botaoSim');
+        let botaoNao = document.getElementById('botaoNao');
+    
+    //Aba diálogo:
+        let texto1 = document.getElementById('textoConsoleL1');
+        let texto2 = document.getElementById('textoConsoleL2');
+        let texto3 = document.getElementById('textoConsoleL3');
+
+//Funções que serão utilizadas durante o jogo:
+
+    //Alterar Aba do dano mostrado na tela:
+      //  function alterarDano() {
+       //     abaDano.innerHTML = 
+      //  }
+
+      function cliqueSim() {
+          resposta = "SIM";
+      }
+      
+      function cliqueNao() {
+        resposta = "NAO";
+    }
+
+//Iniciando a história do game:
+        texto1.innerHTML = `Você é um guerreiro chamado Dragonborn, que decidiu se aventurar pelo mundo para se tornar o maior de todos os guerreiros.`;
+        texto2.innerHTML = `Durante o caminho, você avistou uma árvore de maçã, deseja comer uma maçã?`;
+        texto3.style.display = 'none';
+
+        botoesAcao.style.display = 'block';
+        if(resposta == "SIM") {
+            texto1.innerHTML = `Você decidiu comer a maçã, ela estava saborosa!!!.`;
+            texto2.innerHTML = `Sua vida aumentou em + 10 :D`;
+            texto3.style.display = 'none';
+            hp = hp + 10;
+        }
+
+    /*debug*/
+   // console.log(dano);
+    //alert("funcionando")
+}
